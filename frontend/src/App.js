203b0f4b3a1e293681e6b6e2e2d1d6d2655ce79c
Dylserker <levant.dylan@gmail.com>
2025-04-10 16:23:22 +0200

    Register front terminer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './pages/Register';
-import Login from './pages/Login';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
-                <main>
-                    <Routes>
-                        <Route path="/" element={<Register />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/login" element={<Login />} />
-                    </Routes>
-                </main>
-            </BrowserRouter>
         </div>
     );
 }
