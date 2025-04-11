d856f0b9422e150c8ad502be10022054b95425ca
Dylserker <levant.dylan@gmail.com>
2025-04-11 09:10:32 +0200

    logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import Logo from '../assets/logo/Logo_without_back.png';
+import Logo from '../assets/logo/Logo_sans_titre.png';
 
 function Header() {
     return (
