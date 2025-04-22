7a2f4935d12e1efbd817d0818b39db450d27fb34
Dylserker <levant.dylan@gmail.com>
2025-04-22 09:11:06 +0200

    redirection profile via le header

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
 import logoImage from '../../assets/logo/Logo_sans_titre.png';
 import ProgressBar from '../ProgressBar';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleProfileClick = () => {
+        navigate('/profile');
+    };
+
     const user = {
         pseudo: "Utilisateur",
         level: 5,
@@ -37,7 +43,7 @@ const Header = () => {
                         <ProgressBar progress={user.progress} />
                     </div>
                     <div className="profile-photo-container">
-                        <div className="profile-photo"></div>
+                        <div className="profile-photo" onClick={handleProfileClick}></div>
                     </div>
                 </div>
             </div>
