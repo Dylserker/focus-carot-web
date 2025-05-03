110d86aee59ef6f77e1ccb7dd445f12a8602c667
Dylserker <levant.dylan@gmail.com>
2025-05-03 21:07:14 +0200

    commentaire enlever

---
+++ backend/src/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'focus_carot_web',
+        'user' => 'root',
+        'pass' => ''
+    ],
+    'jwt' => [
+        'secret' => 'votre_clé_secrète_jwt'
+    ]
+];
