000914807ed3196104b4af29cfc217460f8887c2
Dylserker <levant.dylan@gmail.com>
2025-05-04 03:58:18 +0200

    commentaire enlever

--- backend/index.php
+++ backend/index.php
@@ -3,7 +3,6 @@
 use App\Controllers\HomeController;
 use App\Controllers\UsersController;
 use App\Core\Routeur;
-use App\Core\TemplateEngine;
 use App\Kernel;
 
 require 'vendor/autoload.php';
@@ -12,5 +11,6 @@ $routeur = new Routeur();
 $routeur->addRoute(['GET'], '/users/{id}', UsersController::class, 'user');
 $routeur->addRoute(['GET'], '/', HomeController::class, 'index');
 $routeur->addRoute(['GET'], '/users', UsersController::class, 'liste');
+$routeur->addRoute(['POST'], '/api/login', UsersController::class, 'connexion');
 
 new Kernel($routeur);
