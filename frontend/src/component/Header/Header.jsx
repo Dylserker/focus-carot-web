f65df2947eb5782c64e87ec949510a9d2d3fad4f
Dylserker <levant.dylan@gmail.com>
2025-05-22 13:30:30 +0200

    CSS progressbar header

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
 import logoImage from '../../assets/logo/Logo_sans_titre.png';
-import ProgressBar from '../ProgressBar';
+import ProgressBar from '../ProgressBar/ProgressBar';
 import { useAuth } from '../../contexts/AuthContext';
 
 const Header = () => {
