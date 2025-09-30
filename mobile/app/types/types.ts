f99fc5ed7ae141b86edd2ef35751d38089cb5221
Dylserker <levant.dylan@gmail.com>
2025-09-30 13:29:36 +0200

    Initialisation frontend react ts + vite

--- mobile/app/types/types.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    photoUrl?: string;
-    title?: string;
-    username: string;
-    firstName: string;
-    lastName: string;
-    birthDate: string | Date;
-    email: string;
-}
